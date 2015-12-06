ea950660045e781c7a257d01eca175cb858b8db7
Julin Maloof <jnmaloof@ucdavis.edu>
2015-12-06 15:24:21 -0800

    added methods for counting reads in simulated fasta file

---
+++ CountGene.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+
+#script to count the number of occurences of genes in a fasta file generated from polyester
+#Julin Maloof
+#Dec 6, 2015
+
+import sys, re
+
+from collections import Counter
+
+if len(sys.argv)  != 2 :
+    print("This script takes fasta file generated from polyester and counts gene occurences")
+    print("It currently is hard-wired for Slyc and Spen, looking for the following pattern")
+    print("'Sopen.+Solyc[0-9]{2}g[0-9\\.]+'")
+    exit()
+
+count = Counter()
+
+f = open(sys.argv[1],'r')
+
+for line in f:
+    gene = re.search('Sopen.+Solyc[0-9]{2}g[0-9\\.]+',line)
+    if gene:
+        count[gene.group(0)] += 1
+
+for gene, count in count.items():
+    print(gene,count)
