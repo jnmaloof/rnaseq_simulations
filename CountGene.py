c7fd30fe114bd83f8dfdd94f300e66f40c047687
Julin Maloof <jnmaloof@ucdavis.edu>
2015-12-17 17:21:16 -0800

    Updated CountGenes.py to accept .gzip or standard files

--- CountGene.py
+++ CountGene.py
@@ -4,19 +4,26 @@
 #Julin Maloof
 #Dec 6, 2015
 
-import sys, re
+import sys, re, gzip
+
+from pathlib import PurePath
 
 from collections import Counter
 
 if len(sys.argv)  != 2 :
-    print("This script takes fasta file generated from polyester and counts gene occurences")
+    print("This script takes a fasta or fasta.gz file generated from polyester and counts gene occurences")
     print("It currently is hard-wired for Slyc and Spen, looking for the following pattern")
     print("'Sopen.+Solyc[0-9]{2}g[0-9\\.]+'")
+    print("Counts are output to standard out")
     exit()
 
 count = Counter()
 
-f = open(sys.argv[1],'r')
+
+if PurePath(sys.argv[1]).suffix == '.gz' :
+    f = gzip.open(sys.argv[1],'rt')
+else :
+    f = open(sys.argv[1],'r')
 
 for line in f:
     gene = re.search('Sopen.+Solyc[0-9]{2}g[0-9\\.]+',line)
